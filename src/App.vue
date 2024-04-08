353527c9af26c23d3f526473669c92d17d2f7837
Julien Minguely <jminguely@gmail.com>
2024-04-09 00:48:26 +0200

    Use svh for the site height

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col min-h-screen">
+  <div class="flex flex-col min-h-svh">
     <header class="text-center p-5 bg-red-700 border-b-2">
       <h1 class="text-3xl font-bold uppercase">Conju-baguette</h1>
     </header>
