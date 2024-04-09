d00d0076add5686dc373d1a371bab42f4672d12b
Julien Minguely <jminguely@gmail.com>
2024-04-10 00:34:17 +0200

    Update CI for api

--- api/app.py
+++ api/app.py
@@ -1,15 +1,10 @@
-from flask import Flask, jsonify, request
+from fastapi import FastAPI
 from verbecc import Conjugator
 
-app = Flask(__name__)
+app = FastAPI()
 cj = Conjugator("fr")
 
-@app.route('/conjugate/<verb>', methods=['GET'])
-def conjugate(verb):
-    if not verb:
-        return jsonify({'error': 'The verb parameter is required.'}), 400
+@app.get('/conjugate/{verb}')
+async def conjugate(verb: str):
     conjugation = cj.conjugate(verb)
-    return jsonify(conjugation)
-
-if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=8080)
+    return conjugation
