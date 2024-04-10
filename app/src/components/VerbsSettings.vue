ded0186d99852dff69971a5e08af2a0d65bc232f
Julien Minguely <jminguely@gmail.com>
2024-04-10 13:41:20 +0200

    fix verbs display on small mobile

--- app/src/components/VerbsSettings.vue
+++ app/src/components/VerbsSettings.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="flex flex-col">
     <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-      <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+      <div class="py-2 align-middle inline-block min-w-full sm:px-2 lg:px-8">
         <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
           <table class="min-w-full divide-y divide-gray-200">
             <thead class="bg-gray-50">
               <tr>
                 <th
                   scope="col"
-                  class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  class="px-2 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                 >
                   French
                 </th>
                 <th
                   scope="col"
-                  class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  class="px-2 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                 >
                   Spanish
                 </th>
@@ -22,10 +22,10 @@
             </thead>
             <tbody class="bg-white divide-y divide-gray-200">
               <tr v-for="(verb, index) in availableVerbs" :key="index">
-                <td class="px-6 py-4 whitespace-nowrap">
+                <td class="px-2 py-4 whitespace-nowrap">
                   {{ verb.fr }}
                 </td>
-                <td class="px-6 py-4 whitespace-nowrap">
+                <td class="px-2 py-4 whitespace-nowrap">
                   {{ verb.es }}
                 </td>
               </tr>
