8ac6352a63d0a0d0137f62294c1d6cddeaa3e303
Julien Minguely <jminguely@gmail.com>
2024-05-19 10:53:00 +0200

    Revert url fixing

--- api/app.py
+++ api/app.py
@@ -1,10 +1,10 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI
 from verbecc import Conjugator
 
 app = FastAPI()
 
 @app.get('/conjugate/{lang}/{verb}')
-async def conjugate(lang: str = Query(...), verb: str = Query(...)):
+async def conjugate(lang: str, verb: str):
     cj = Conjugator(lang)
     conjugation = cj.conjugate(verb)
     return conjugation
